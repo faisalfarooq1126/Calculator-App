7e96baff2740a8aa5b93a0c4a6b3b285583a4ef7
faisal <faisalfaroqq1873@gmail.com>
2025-09-30 14:36:06 +0500

    create a file

---
+++ app.js
@@ -0,0 +1,39 @@
+
+var input = document.getElementById("inputResult");
+
+// function getValue(value) {
+//   input.value = input.value + value;
+// }
+
+function getValue(value) {
+  var operators = ["*", "-", "+", "/", "%"];
+  var last = input.value.slice(-1);
+
+  // Case 1: Agar last bhi operator hai aur naya bhi operator hai
+  if (operators.includes(last) && operators.includes(value)) {
+    return;
+  }
+
+  // Case 2: Baaki sab normal
+  input.value = input.value + value;
+}
+
+function clrAll() {
+  input.value = "";
+}
+
+function square() {
+  input.value = input.value * input.value;
+}
+
+function deleteChar() {
+  input.value = input.value.slice(0, -1);
+}
+
+function equal() {
+  input.value = eval(input.value);
+}
+
+function fractions() {
+  input.value = 1 / input.value;
+}
